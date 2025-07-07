689ffbe11ae5b354ae2bfa9247134dedb03b3bf2
fetehadin <fetehadinnegash@gmail.com>
2025-07-07 07:34:08 +0300

    Update subqueries.sql

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,1 +1,29 @@
+-- Non-Correlated Subquery
+-- Find all properties with an average rating > 4.0
+
+SELECT 
+  Property.property_id,
+  Property.name,
+  Property.location
+FROM Property
+WHERE Property.property_id IN (
+  SELECT Review.property_id
+  FROM Review
+  GROUP BY Review.property_id
+  HAVING AVG(Review.rating) > 4.0
+);
+
+-- Correlated Subquery
+-- Find users who have made more than 3 bookings
+
+SELECT 
+  User.user_id,
+  User.first_name,
+  User.email
+FROM User
+WHERE (
+  SELECT COUNT(*) 
+  FROM Booking 
+  WHERE Booking.user_id = User.user_id
+) > 3;
 
