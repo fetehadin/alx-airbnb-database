9ffd35b78757d443bd9bc3449c0aa6ea2382b87e
fetehadin <fetehadinnegash@gmail.com>
2025-07-07 07:31:09 +0300

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,1 +1,42 @@
+-- INNER JOIN
+SELECT 
+  Booking.booking_id,
+  Booking.start_date, 
+  Booking.end_date,
+  User.user_id,
+  User.email
+FROM Booking
+INNER JOIN User ON Booking.user_id = User.user_id;
+
+-- LEFT JOIN
+SELECT
+  Property.property_id,
+  Property.name,
+  Property.location,
+  Review.review_id,
+  Review.rating
+FROM Property
+LEFT JOIN Review ON Property.property_id = Review.property_id
+ORDER BY Property.property_id;
+
+-- FULL OUTER JOIN (using UNION for MySQL)
+SELECT 
+  User.user_id,
+  User.email,
+  Booking.booking_id,
+  Booking.start_date,
+  Booking.end_date
+FROM User
+LEFT JOIN Booking ON User.user_id = Booking.user_id
+
+UNION
+
+SELECT 
+  Booking.booking_id,
+  Booking.start_date, 
+  Booking.end_date,
+  User.user_id,
+  User.email
+FROM Booking
+RIGHT JOIN User ON Booking.user_id = User.user_id;
 
